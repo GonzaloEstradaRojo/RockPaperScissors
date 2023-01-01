cb5bdb880bee4e3af214a5ee76b4a012437c713f
Gonzalo Estrada Rojo <48635918+GonzaloEstradaRojo@users.noreply.github.com>
2023-01-01 20:30:54 +0100

    Main logic updated

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,44 +1,53 @@
 import pygame
 import random
 import math
+import PieceModule as pm
 
 # Inicializa pygame y crea una ventana
 pygame.init()
 width, height = 800, 800
-velMovement = 20
-radiusW = 20
+velMovement = 10
+iconSize = 30
 run = True
 screen = pygame.display.set_mode((width, height))
+SCISSORS = "scissors.png"
+PAPER = "paper.png"
+ROCK = "rock.png"
+frameRate = 15
 
-circle = [width//2,height//2]
+pygame.init()
 
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('A bit Racey')
 
-while run:
-    
-    # Procesa eventos de pygame
+clock = pygame.time.Clock()
+running = True
+
+carImg = pygame.image.load('rock.png')
+carImg = pygame.transform.scale(carImg, (iconSize, iconSize))
+
+p1 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
+ velMovement, iconSize, SCISSORS)
+p2 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
+ velMovement, iconSize, ROCK)
+p3 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
+ velMovement, iconSize, PAPER)
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-    
-    # 0 = Up  1 = Rigth  2 = Down  3 = Left
-    dir = math.floor(random.uniform(0,4))
+            running = False
 
-    # if(dir == 0): #UP
-    #     if((circle[1]-radiusW//2) > 0):
-    #         circle[1] -= velMovement  
-    # elif(dir == 1): #RIGTH
-    #     if((circle[0]+radiusW//2) < width):
-    #         circle[0] += velMovement 
-    # elif(dir == 2): #DOWN
-    #     if((circle[1]+radiusW//2) < height):
-    #         circle[1] += velMovement  
-    # elif(dir == 3): #LEFT
-    #     if((circle[0]-radiusW//2) > 0):
-    #         circle[0] -= velMovement 
-        
+    screen.fill((255,255,255))
+    p1.MovePiece(screen)
+    p1.DrawPiece(screen)
+    p2.MovePiece(screen)
+    p2.DrawPiece(screen)
+    p3.MovePiece(screen)
+    p3.DrawPiece(screen)
 
-    screen.fill((255, 255, 255))
-    
-    pygame.draw.circle(screen, (0, 0, 0), (int(circle[0]), int(circle[1])), radiusW)
-    
     pygame.display.update()
+    clock.tick(frameRate)
+
+pygame.quit()
+quit()
