608bb8c389245a067c3fdedba626d79a06d727ac
Gonzalo Estrada Rojo <48635918+GonzaloEstradaRojo@users.noreply.github.com>
2023-01-02 03:11:55 +0100

    File modified to use sprite class instead of blitz

--- PieceModule.py
+++ PieceModule.py
@@ -2,42 +2,86 @@ import pygame
 import random
 import math
 
-class Piece:
-    def __init__(self, pos, vel, iconSize, icon) -> None:
-        self._pos = pos
-        self._posX = pos[0]
-        self._posY = pos[1]
-        self._vel = vel
-        self._iconSize = iconSize
-        self._icon = icon
-        self._image = pygame.transform.scale(pygame.image.load(icon), (iconSize, iconSize))
+# class Piece:
+#     def __init__(self, name, pos, vel, iconSize, icon) -> None:
+#         self._pos = pos
+#         self._posX = pos[0]
+#         self._posY = pos[1]
+#         self._vel = vel
+#         self._iconSize = iconSize
+#         self._icon = icon
+#         self._image = pygame.transform.scale(pygame.image.load(icon), (iconSize, iconSize))
+#         self._rect = self._image.get_rect()
+#         self._name = name
+
+#     def GenerateSeed(self):
+#         return math.floor(random.uniform(0,4))   
+
+#     def MovePiece(self, screen):
+#         w, h = screen.get_size()
+#         seed = self.GenerateSeed()
+#         if(seed == 0): #LEFT
+#             if((self._posX) > 0):
+#                 self._posX -= self._vel 
+#         elif(seed == 1): #RIGTH
+#             if((self._posX + self._iconSize) <= w):
+#                 self._posX += self._vel  
+#         elif(seed == 2): #UP
+#             if((self._posY) > 0):
+#                 self._posY -= self._vel   
+#         elif(seed == 3): #DOWN
+#             if((self._posY + self._iconSize) < h):
+#                 self._posY += self._vel   
+    
+#     def DrawPiece(self, screen):
+#         image = pygame.image.load(self._icon)
+#         rectangle = pygame.Rect(self._posX, self._posY, self._iconSize, self._iconSize)
+
+#         pygame.draw.rect(screen, (0,0,0), rectangle)
+#         screen.blit(self._image, (self._posX, self._posY))
+
+#     def CheckCollision(self, collisionList):
+#         # print("type ", type(self._rect))
+#         # print(dir(self._rect))
+#         # result = self._rect.collideobjects(collisionList)
+#         # print(f"La pieza {self._name} se ha chocado con {result._name}")
+#         pass
+
+#     def ChangeIcon(self):
+#         pass
+
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, name, pos, vel, iconSize, icon) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load(icon), (iconSize, iconSize))
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.vel = vel
+
+        self.pos = pos
+        self.posX = pos[0]
+        self.posY = pos[1]
+        self.icon = icon
+        self.iconSize = iconSize
+        self.name = name
 
     def GenerateSeed(self):
         return math.floor(random.uniform(0,4))   
 
-    def MovePiece(self, screen):
+    def update(self, screen):
         w, h = screen.get_size()
         seed = self.GenerateSeed()
         if(seed == 0): #LEFT
-            if((self._posX) > 0):
-                self._posX -= self._vel 
+            if((self.rect.x) > 0):
+                self.rect.x -= self.vel 
         elif(seed == 1): #RIGTH
-            if((self._posX + self._iconSize) <= w):
-                self._posX += self._vel  
+            if((self.rect.x + self.iconSize) <= w):
+                self.rect.x += self.vel  
         elif(seed == 2): #UP
-            if((self._posY) > 0):
-                self._posY -= self._vel   
+            if((self.rect.y) > 0):
+                self.rect.y -= self.vel   
         elif(seed == 3): #DOWN
-            if((self._posY + self._iconSize) < h):
-                self._posY += self._vel   
+            if((self.rect.y + self.iconSize) < h):
+                self.rect.y += self.vel   
     
-    def DrawPiece(self, screen):
-        image = pygame.image.load(self._icon)
-        rectangle = pygame.Rect(self._posX, self._posY, self._iconSize, self._iconSize)
-
-        pygame.draw.rect(screen, (0,0,0), rectangle)
-        screen.blit(self._image, (self._posX, self._posY))
-
-    def CheckCollision(self):
-        pass
-
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,35 +3,50 @@ import random
 import math
 import PieceModule as pm
 
+print(pygame.__version__)
 # Inicializa pygame y crea una ventana
 pygame.init()
 width, height = 800, 800
-velMovement = 10
+velMovement = 15
 iconSize = 30
+numPieces = 30
+frameRate = 10
 run = True
 screen = pygame.display.set_mode((width, height))
 SCISSORS = "scissors.png"
 PAPER = "paper.png"
 ROCK = "rock.png"
-frameRate = 15
 
 pygame.init()
 
 screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Rock Paper Scissor')
 
 clock = pygame.time.Clock()
 running = True
 
-carImg = pygame.image.load('rock.png')
-carImg = pygame.transform.scale(carImg, (iconSize, iconSize))
+scissorsGroup = pygame.sprite.Group(())
+rockGroup = pygame.sprite.Group(())
+paperGroup = pygame.sprite.Group(())
+allSpritesGroup = pygame.sprite.Group(())
 
-p1 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
- velMovement, iconSize, SCISSORS)
-p2 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
- velMovement, iconSize, ROCK)
-p3 = pm.Piece([random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],
- velMovement, iconSize, PAPER)
+for i in range(numPieces):
+    prob = math.floor(random.uniform(0,3))
+    pieza = None
+    if(prob == 0):
+        pieza = pm.Piece(f"S{i}",[random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],velMovement, iconSize, SCISSORS)
+        pieza.add(scissorsGroup,allSpritesGroup)
+    elif(prob == 1):
+        pieza = pm.Piece(f"R{i}",[random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],velMovement, iconSize, ROCK)
+        pieza.add(rockGroup,allSpritesGroup)
+    elif(prob == 2):
+        pieza = pm.Piece(f"P{i}",[random.uniform(0, width-iconSize),random.uniform(0, height-iconSize)],velMovement, iconSize, PAPER)
+        pieza.add(paperGroup,allSpritesGroup)
+
+# pieceList = []
+# pieceList.append(p1)
+# pieceList.append(p2)
+# pieceList.append(p3)
 
 while running:
     for event in pygame.event.get():
@@ -39,15 +54,21 @@ while running:
             running = False
 
     screen.fill((255,255,255))
-    p1.MovePiece(screen)
-    p1.DrawPiece(screen)
-    p2.MovePiece(screen)
-    p2.DrawPiece(screen)
-    p3.MovePiece(screen)
-    p3.DrawPiece(screen)
+    allSpritesGroup.update(screen)
+    # p1.MovePiece(screen)
+    # p2.MovePiece(screen)
+    # p3.MovePiece(screen)
+
+    allSpritesGroup.draw(screen)
+    # p1.DrawPiece(screen)
+    # p2.DrawPiece(screen)
+    # p3.DrawPiece(screen)
+
+    # p1.CheckCollision(pieceList)
+    # p2.CheckCollision(pieceList)
+    # p3.CheckCollision(pieceList)
 
     pygame.display.update()
     clock.tick(frameRate)
-
 pygame.quit()
 quit()
